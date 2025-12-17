05637619102c69bf9bb53c41d4ee26ebdc7f5ff2
Joshua Candia <jcandia@reinoceramicos.com.ar>
2025-12-17 15:28:30 -0300

    add logger in index

--- src/controllers/whatsapp.controller.js
+++ src/controllers/whatsapp.controller.js
@@ -1,11 +1,11 @@
 const verifyToken = (req, res) => {
   const VERIFY_TOKEN = process.env.META_VERIFY_TOKEN;
 
-  const mode = req.query["hub.mode"];
+  // const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+  if (challenge !== null && token !== null && token === VERIFY_TOKEN) {
     return res.status(200).send(challenge);
   }
 
--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const apiRoutes = require("./routes/route");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use("/webhook", apiRoutes);
+app.use(express.json());
+
+app.use((req, _res, next) => {
+  if (req.path.startsWith("/webhook")) {
+    console.log("IN", req.method, req.originalUrl, req.headers["user-agent"]);
+  }
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

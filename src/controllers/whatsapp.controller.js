d62a8b3bf412b4c99732a741a202eaa1b334312b
Joshua Candia <jcandia@reinoceramicos.com.ar>
2025-12-17 12:42:42 -0300

    refactor: improve token verification logic in whatsapp.controller

--- src/controllers/whatsapp.controller.js
+++ src/controllers/whatsapp.controller.js
@@ -1,5 +1,17 @@
 const verifyToken = (req, res) => {
-  res.send("Hola verify token");
+  try {
+    const accessToken = process.env.META_ACCESS_TOKEN;
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (challenge && token && token === accessToken) {
+      res.status(200).send(challenge);
+    } else {
+      res.status(400).send("Token de verificación no válido");
+    }
+  } catch (error) {
+    res.status(400).send("Error en la verificación del token");
+  }
 };
 
 const receiveMessage = (req, res) => {
